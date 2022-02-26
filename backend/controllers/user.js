73523854a14e645723c0d8faf0486a477302235c
Aude-Melanie NYOUNG <maud.nyoung@gmail.com>
2022-02-26 17:16:54 +0100

    ROUTE POST Login = OK

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,6 @@
 const bcrypt = require('bcrypt');
 const sequelize = require('../sequelize');
-//const jwt = require('jsonwebtoken');
-
-const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res) => {
   console.log(sequelize.models);
@@ -26,30 +24,43 @@ exports.signup = (req, res) => {
       console.error(error);
       res.status(500).json({ message: 'Erreur SERVEUR' })
     })
-  };
+};
 
-/*exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          return res.status(401).json({ message: 'Utilisateur non trouvé !' });
-        }
-        bcrypt.compare(req.body.password, user.password)
-          .then(valid => {
-            if (!valid) {
-              return res.status(401).json({ message: 'Mot de passe incorrect !' });
-            }
-            res.status(200).json({
-              userId: user._id,
-              token: jwt.sign(
-                { userId: user._id },
-                'RANDOM_TOKEN_SECRET',
-                { expiresIn: '24h' }
-              )
-            });
-          })
-          .catch(error => res.status(500).json({ message: error }));
-      })
-      .catch(error => res.status(500).json({ message: error }));
-  };
-*/
+exports.login = (req, res) => {
+  console.log(sequelize.models)
+
+  sequelize.models.User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+
+        return res.status(401).json({ message: "Utilisateur non trouvé" })
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            
+            return res.status(401).json({ message: "Mot de passe incorrect" })
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              'RANDOM_TOKEN_SECRET',
+              { expiresIn: '24h' }
+            )
+          });
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).json({ message: 'Erreur SERVEUR' })
+        })
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).json({ message: 'Erreur SERVEUR' })
+    })
+};
+
+exports.deleteAccount = (req, res) => {
+
+};
