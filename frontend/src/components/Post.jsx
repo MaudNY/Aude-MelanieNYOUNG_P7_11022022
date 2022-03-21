ef440a92d20935b908b7d05ab5bce288873c0876
Aude-Melanie NYOUNG <maud.nyoung@gmail.com>
2022-03-21 16:29:00 +0100

    Homepage - Rajout métier + ligne réactions à post

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CreateComment from './CreateComment';
 import DeleteIcon from '@mui/icons-material/Delete';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 
 export default function Post() {
   return (
@@ -10,15 +11,26 @@ export default function Post() {
                 <img src="./assets/man-woman-looking(large).jpg" alt="logo" />
             </div>
             <div className="post-details">
-                <div className="author-first-name">Li</div>
-                <div className="author-last-name">Mai</div>
-                <div className="post-date">Publié le 01/01/1970 à 15:59</div>
+                <div className="author-details">
+                    <div className="author-name">
+                        <div className="author-first-name">Li</div>
+                        <div className="author-last-name">Mai</div>
+                    </div>
+                    <div className="author-job">Sales Director</div>
+                </div>
+                <div className="post-date">01/01/1970 à 15:59</div>
                 <DeleteIcon className="delete-icon" />
             </div>
         </div>
         <div className="post-line-two post-content">Blablabla</div>
         <div className="post-line-three">
             <img src="./assets/man-woman-looking(large).jpg" alt="logo" />
+        </div>
+        <div className="post-line-reactions">
+            <div className="comment-section">
+                <ChatBubbleOutlineIcon className="comment-icon"/>
+                <div className="comment-description">0 commentaires</div>
+            </div>
         </div>
         <div className="post-last-line">
             <CreateComment />
